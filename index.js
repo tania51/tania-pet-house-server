39b00a03cfbe99da0350cea1c50f29969965ce3c
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2023-11-26 05:02:26 +0600

    create allPet, adoptPet, paymetIntent api and adopted pet using post method, payment intent with post method, get all pet using get method

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_PAYMENT_KEY)
+
+const cors = require('cors')
+const port = process.env.PORT || 5008
+
+// parser
+app.use(cors())
+app.use(express.json())
+
+
+// mongodb setup
+
+
+const uri = "mongodb+srv://petAdoption:c9VkMSd4eNWR4rJG@cluster0.jwathvu.mongodb.net/?retryWrites=true&w=majority"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const petCollection = client.db('petAdoption').collection('allPet')
+    const adoptedPetCollection = client.db('petAdoption').collection('adoptedPet')
+    const paymentsCollection = client.db('petAdoption').collection('payments')
+
+
+    app.get('/all-pets', async (req, res) => {
+      const result = await petCollection.find().toArray();
+
+      res.send(result)
+    })
+
+    app.get('/all-pets/petDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await petCollection.findOne(query)
+      // console.log(result);
+      res.send(result)
+    })
+
+    // adopted pet post
+    app.post('/adoptedPet', async (req, res) => {
+      const query = req.body;
+      const result = await adoptedPetCollection.insertOne(query);
+      res.send(result)
+    })
+
+    // stripe payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+        })
+
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+
+    })
+
+
+    // save payment info using post method
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+
+      // update price by payment amount
+      const filter = { _id: new ObjectId(payment.pet_obj_id) }
+
+      const updateDoc = {
+        $set: {
+          donated_amount: payment.total_donation_amount
+        }
+      }
+
+      const result = await petCollection.updateOne(filter, updateDoc)
+
+      const paymentResult = await paymentsCollection.insertOne(payment)
+      res.send({ paymentResult, result })
+    })
+
+    // get all the donation campaigns from paymentsCollection
+    app.get('/donation-campaigns/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const query = { email: email }
+      const result = await paymentsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Pet server working perfectly')
+})
+
+app.listen(port, () => {
+  console.log(`Pet server is running on ${port}`)
+})
