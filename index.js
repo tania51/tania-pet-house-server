812f54a08c815394e86a373a505c78bebb016761
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2023-12-29 23:49:32 +0600

    change repository private to public

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const cors = require('cors')
 const port = process.env.PORT || 5008
 
 // parser
-app.use(cors())
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://assignment-12-57b4f.web.app'
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 
 
@@ -153,15 +162,14 @@ async function run() {
     //is Admin check
     app.get('/users/admin/:email', async(req, res) => {
       const email = req.params.email;
-      console.log(email);
       // if(email !== req.decoded.email) {
       //   return res.status(403).send({message : 'unAuthorized'})
       // }
       const query = {email : email} 
       const user = await usersCollection.findOne(query);
-      if(user) {
-        admin = user?.role === 'admin'
-      }
+      
+        const admin = user?.role === 'admin'
+      
       // console.log(admin);
       res.send({admin})
 
@@ -403,8 +411,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
